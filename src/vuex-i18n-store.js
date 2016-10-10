f2298c5c18e76ddc00d332d8c08b9c7bf8a258f0
tikiatua <tikiatua@gmail.com>
2016-10-10 08:51:10 +0200

    enable usage of plugin directly in browser or with module loaders

--- src/vuex-i18n-store.js
+++ src/vuex-i18n-store.js
@@ -1,39 +1,71 @@
-// define a simple module to handle locales
-const i18nVuexModule = {
-	state: {
-		locale: null,
-		translations: {}
-	},
-	mutations: {
+/* vuex-i18n-store defines a vuex module to store locale translations. Make sure
+** to also include the file vuex-i18n.js to enable easy access to localized
+** strings in your vue components.
+*/
 
-		// set the current locale
-		SET_LOCALE(state, payload) {
-			state.locale = payload.locale;
-		},
+// make sure the plugin can be used directly in the browser or with an es6 module
+// loader. more information on this can be found here:
+// http://ifandelse.com/its-not-hard-making-your-library-support-amd-and-commonjs/
 
-		// add a new locale
-		ADD_LOCALE(state, payload) {
-			state.translations[payload.locale] = payload.translations;
-		}
+(function(root, factory) {
 
-	},
-	actions: {
+	if (typeof define === 'function' && define.amd) {
+		// support amd loaders
+		define(['plugin'], function(plugin) {
+			return (root.i18nVuexModule = factory(plugin));
+		});
 
-		// set the current locale
-		setLocale(context, payload) {
-			context.commit('SET_LOCALE', payload);
-		},
+	} else if(typeof module === 'object' && module.exports) {
+		// support commonjs loaders
+		module.exports = (root.i18nVuexModule = factory(require('plugin')));
 
-		// add a new locale with translations
-		addLocale(context, payload) {
-			context.commit({
-				type: 'ADD_LOCALE',
-				locale: payload.locale,
-				translations: payload.translations
-			});
-		}
+	} else {
+		// support inclusion directly in the browser
+		root.i18nVuexModule = factory(root.plugin);
 
 	}
-};
 
-export default i18nVuexModule;
+}(this, function(plugin) {
+
+	// define a simple vuex module to handle locale translations
+	return {
+		state: {
+			locale: null,
+			translations: {}
+		},
+		mutations: {
+
+			// set the current locale
+			SET_LOCALE(state, payload) {
+				state.locale = payload.locale;
+			},
+
+			// add a new locale
+			ADD_LOCALE(state, payload) {
+				state.translations[payload.locale] = payload.translations;
+			}
+
+		},
+		actions: {
+
+			// set the current locale
+			setLocale(context, payload) {
+				context.commit({
+					type: 'SET_LOCALE',
+					locale: payload.locale
+				});
+			},
+
+			// add a new locale with translations
+			addLocale(context, payload) {
+				context.commit({
+					type: 'ADD_LOCALE',
+					locale: payload.locale,
+					translations: payload.translations
+				});
+			}
+
+		}
+	};
+
+}));
